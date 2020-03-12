019ddf8bb25538cbc1b11ba62e230cf9d3a1e0c1
Rafael Garcia <rafagrcb@gmail.com>
2020-03-12 17:04:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
