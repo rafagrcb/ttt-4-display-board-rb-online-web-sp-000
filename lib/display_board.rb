3f8cc5c141f435635a888fcbfd8bd390221a25b3
Rafael Garcia <rafagrcb@gmail.com>
2020-03-12 17:59:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
 
 def display_board(board)
-  puts row = " #{board[0]} | #{board[1]} | #{board[2]}
-  puts -------------"
+  puts row = " #{board[0]} | #{board[1]} | #{board[2]} "
+  -------------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-------------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
